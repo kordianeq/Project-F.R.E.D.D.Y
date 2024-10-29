f89409e803215b11ab05236f2d9bc0c9611b61b3
t0rf1 <kacperstrzelczyk04@gmail.com>
2024-10-29 12:30:26 +0100

    Model + animations
    added model and walking animations, rn no colliders on main map, player falls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController characterController;
+    private Animator animator;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = .5f;
+    private float dampTime = 0.1f;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,8 +22,25 @@ public class Player : MonoBehaviour
         Vector2 movementVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(movementVector.x, 0f, movementVector.y);
 
+        if (movementVector.magnitude != 0f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else animator.SetBool("isWalking", false);
+
+        animator.SetFloat("x", movementVector.x, dampTime, Time.deltaTime);
+        animator.SetFloat("y", movementVector.y, dampTime, Time.deltaTime);
+
         Vector2 rotateVector = gameInput.GetRotationVector();
         transform.Rotate(0, rotateVector.x * rotateSpeed, 0);
+
+        if (rotateVector.x != 0f)
+        {
+            animator.SetBool("isTurning", true);
+        }
+        else animator.SetBool("isTurning", false);
+
+        animator.SetFloat("turn", rotateVector.x, dampTime, Time.deltaTime);
 
         moveDirection = transform.TransformDirection(moveDirection);
         characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
